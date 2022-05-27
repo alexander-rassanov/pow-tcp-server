b5c6905e92a24e8e9926910a4b70cd7e26faaaab
Alexander Rassanov <alexander.rassanov@gmail.com>
2022-05-27 14:01:52 +0300

    added doc

---
+++ pkg/pow/doc.go
@@ -0,0 +1,5 @@
+// Package pow provides client and server utils to decorate your service to protect against spam/DDoS/etc.
+// It provides Hashcash algorithm, more information you can read here https://en.wikipedia.org/wiki/Hashcash.
+// The package provides client and server capabilities to protect your service by challenge-response protocol.
+// See more information about the protocol here https://en.wikipedia.org/wiki/Proof_of_work.
+package pow
